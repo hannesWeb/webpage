98147e76dda309e7028567969b1d732c2357789e
Hannes Hirsch <hannesbusiness@yahoo.com>
2024-08-15 18:00:15 +0200

    Update contact.php

--- legal/contact/contact.php
+++ legal/contact/contact.php
@@ -1,15 +1,3 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = htmlspecialchars($_POST['name']);
-  $email = htmlspecialchars($_POST['email']);
-  $subject = htmlspecialchars($_POST['subject']);
-  $message = htmlspecialchars($_POST['message']);
-  $captcha = htmlspecialchars($_POST['captcha']);
-  $question = htmlspecialchars($_POST['question']);
-  
-  eval('$correct_answer = ' . $question . ';');
-}
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -28,52 +16,62 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <body>
     <div class="main_page">
         <div class="page_header">
-            <span>Your message</span>
-        </div>
-        <div class="content_section">
-          <div class="section_header">Your message:</div>
-          <div class="content_section_text">
-              <p><?phpif ($captcha == $correct_answer) {
-    $to = "info@mail.hannes-hirsch.de";
-    $headers = "From: " . $email;
-    
-    mail($to, $subject, $message, $headers);
-    echo "Message sent!";
-  } else {
-    echo "Incorrect CAPTCHA answer. Please try again.";
-  }?></p>
-          </div>
-     </div>    
-</div>
-               <a href="https://www.hannes-hirsch.de" id="floating-button" style="
-    display: block;
-    width: 120px;
-    height: 30px;
-    border-radius: 50%;
-    background-color: #5C6BC0;
-    color: white;
-    text-align: center;
-    padding: 16px 0;
-    position: fixed;
-    bottom: 20px;
-    right: 20px;
-    font-size: 18px;
-    text-decoration: none;
-    transition: background-color 0.3s;
-" onmouseover="this.style.backgroundColor='#3F51B5';" onmouseout="this.style.backgroundColor='#5C6BC0';">
-    Zurück
-    </a>
-<a href="//www.dmca.com/Protection/Status.aspx?ID=42f77e89-fe3a-4c80-9b1c-ef52834c49f0" style="
-    display: block;
-    width: 120px;
-    height: 30px;
-    padding: 16px 0;
-    position: fixed;
-    bottom: 40px;
-    left: 10px;
-    font-size: 18px;
-    text-decoration: none; title="DMCA.com Protection Status" class="dmca-badge"> <img src ="https://images.dmca.com/Badges/DMCA_logo-std-btn180w.png?ID=42f77e89-fe3a-4c80-9b1c-ef52834c49f0"  alt="DMCA.com Protection Status" /></a>  <script src="https://images.dmca.com/Badges/DMCABadgeHelper.min.js"> </script>
+            <span id="message">
+                <?php
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    $name = htmlspecialchars($_POST['name']);
+                    $email = htmlspecialchars($_POST['email']);
+                    $subject = htmlspecialchars($_POST['subject']);
+                    $message = htmlspecialchars($_POST['message']);
+                    $captcha = htmlspecialchars($_POST['captcha']);
+                    $question = htmlspecialchars($_POST['question']);
+                    
+                    eval('$correct_answer = ' . $question . ';');
 
-        </div> 
+                    if ($captcha == $correct_answer) {
+                        $to = "info@mail.hannes-hirsch.de";
+                        $headers = "From: " . $email;
+                        
+                        mail($to, $subject, $message, $headers);
+                        echo "Your message was sent successfully";
+                    } else {
+                        echo "Captcha wrong";
+                    }
+                }
+                ?>
+            </span>
+        </div>
+    </div>
+    <a href="https://www.hannes-hirsch.de" id="floating-button" style="
+        display: block;
+        width: 120px;
+        height: 30px;
+        border-radius: 50%;
+        background-color: #5C6BC0;
+        color: white;
+        text-align: center;
+        padding: 16px 0;
+        position: fixed;
+        bottom: 20px;
+        right: 20px;
+        font-size: 18px;
+        text-decoration: none;
+        transition: background-color 0.3s;
+    " onmouseover="this.style.backgroundColor='#3F51B5';" onmouseout="this.style.backgroundColor='#5C6BC0';">
+        Zurück
+    </a>
+    <a href="//www.dmca.com/Protection/Status.aspx?ID=42f77e89-fe3a-4c80-9b1c-ef52834c49f0" style="
+        display: block;
+        width: 120px;
+        height: 30px;
+        padding: 16px 0;
+        position: fixed;
+        bottom: 40px;
+        left: 10px;
+        font-size: 18px;
+        text-decoration: none;" title="DMCA.com Protection Status" class="dmca-badge">
+        <img src="https://images.dmca.com/Badges/DMCA_logo-std-btn180w.png?ID=42f77e89-fe3a-4c80-9b1c-ef52834c49f0" alt="DMCA.com Protection Status" />
+    </a>
+    <script src="https://images.dmca.com/Badges/DMCABadgeHelper.min.js"></script>
 </body>
 </html>
