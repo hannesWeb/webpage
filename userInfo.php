0e34880733377cb3619eba2e0df609563f2c26f7
Hannes Hirsch <hannesbusiness@yahoo.com>
2024-08-24 12:22:56 +0200

    added newline tags

--- userInfo.php
+++ userInfo.php
@@ -16,14 +16,23 @@ function getVisIpAddr() {
 $ip = getVisIPAddr();
 $ipdat = @json_decode(file_get_contents( 
 	"http://www.geoplugin.net/json.gp?ip=" . $ip)); 
+
 echo 'IP-Adress: ' . $ip . "\n";
+echo "\n";
 echo 'Country Name: ' . $ipdat->geoplugin_countryName . "\n"; 
+echo "\n";
 echo 'City Name: ' . $ipdat->geoplugin_city . "\n"; 
+echo "\n";
 echo 'Continent Name: ' . $ipdat->geoplugin_continentName . "\n"; 
+echo "\n";
 echo 'Latitude: ' . $ipdat->geoplugin_latitude . "\n"; 
+echo "\n";
 echo 'Longitude: ' . $ipdat->geoplugin_longitude . "\n"; 
+echo "\n";
 echo 'Currency Symbol: ' . $ipdat->geoplugin_currencySymbol . "\n"; 
+echo "\n";
 echo 'Currency Code: ' . $ipdat->geoplugin_currencyCode . "\n"; 
+echo "\n";
 echo 'Timezone: ' . $ipdat->geoplugin_timezone; 
 
 ?> 
