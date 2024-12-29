6de18f587011913dca7ebbeafe9670989110702f
Hannes Hirsch <hannesbusiness@yahoo.com>
2024-12-29 12:06:12 +0100

    Update get.php

--- api/get/get.php
+++ api/get/get.php
@@ -3,37 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dateien und Verzeichnisse</title>
+    <title> </title>
     <link rel="stylesheet" href="../../../style.css">
 </head>
 <body>
     <div class="main_page">
         <div class="page_header">
-            <span>Dateien und Verzeichnisse</span>
+            <span>Mögliche Endpunkte</span>
         </div>
         <div class="content_section_text">
-            <p>Hier finden Sie eine Liste aller Dateien und Unterverzeichnisse im aktuellen Verzeichnis:</p>
-            <ul>
-                <?php
-                // Aktuelles Verzeichnis durchgehen
-                $files = scandir('.');
+            <p>Hier finden Sie eine Liste aller möglichen API-GET Endpunkte:</p>
+            <?php
+            // Aktuelles Verzeichnis durchgehen
+            $files = scandir('.');
 
-                // Dateien und Verzeichnisse auflisten
-                foreach ($files as $file) {
-                    // Ignoriere die speziellen Verzeichnisse '.' und '..' sowie 'index.html'
-                    if ($file === '.' || $file === '..' || $file === 'index.html') {
-                        continue;
-                    }
-
-                    // Prüfen, ob es ein Verzeichnis oder eine Datei ist
-                    if (is_dir($file)) {
-                        echo "<li><button onclick=\"location.href='$file'\">[Verzeichnis] $file</button></li>";
-                    } else {
-                        echo "<li><button onclick=\"location.href='$file'\">$file</button></li>";
-                    }
+            // Dateien und Verzeichnisse auflisten
+            foreach ($files as $file) {
+                // Ignoriere die speziellen Verzeichnisse '.' und '..' sowie 'index.html'
+                if ($file === '.' || $file === '..' || $file === 'index.html') {
+                    continue;
                 }
-                ?>
-            </ul>
+
+                // Prüfen, ob es ein Verzeichnis oder eine Datei ist
+                if (is_dir($file)) {
+                    echo "<button onclick=\"location.href='$file'\">[Verzeichnis] $file</button><br>";
+                } else {
+                    echo "<button onclick=\"location.href='$file'\">$file</button><br>";
+                }
+            }
+            ?>
         </div>
     </div>
 </body>
